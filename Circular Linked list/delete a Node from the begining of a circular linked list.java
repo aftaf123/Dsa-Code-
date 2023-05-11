0cff06358f54501fd4f34afd10a8f242ece93528
AFTAF <87799378+aftaf123@users.noreply.github.com>
2023-05-11 17:04:08 +0530

    Add files via upload

---
+++ Circular Linked list/delete a Node from the begining of a circular linked list.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+public class Main{
+    public static Node head = null;
+    public static Node tail = null;
+    class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public void add(int k){
+        Node ss = new Node(k);
+        if(head==null){
+            head=ss;
+            tail=ss;
+            ss.next=head;
+        }
+        else{
+            tail.next=ss;
+            tail=ss;
+            tail.next=head;
+        }
+    }
+    public void deleteFirst(){
+        if(head==null){
+            return;
+        }
+        else{
+            if(head!=tail){
+                head=head.next;
+                tail.next=head;
+            }
+            else{
+                head=tail=null;
+            }
+        }
+    }
+    public void display(){
+        Node curr = head;
+        if(head==null){
+            System.out.println("Empty linked list .....");
+            
+        }
+        else{
+            do{
+                System.out.print(curr.data + " ");
+                curr=curr.next;
+            }while(curr!=head);
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Main m = new Main();
+        System.out.println("Enter length :");
+        int n = sc.nextInt();
+        System.out.println("Enter element :");
+        for(int i =0;i<n;i++){
+            int n1 = sc.nextInt();
+            m.add(n1);
+        }
+        System.out.println("Your linked list is :");
+        m.display();
+        System.out.println("After delete element from the begining...");
+        m.deleteFirst();
+        m.display();
+    }
+}
